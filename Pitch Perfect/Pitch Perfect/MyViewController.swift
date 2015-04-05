c950c1caaa4f40f5bec895a2a240aa0ab97d475e
zohaibmaqsood <maqsood.zohaib@gmail.com>
2015-04-05 15:25:00 -0500

    Committed by Udacity

---
+++ Pitch Perfect/Pitch Perfect/MyViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MyViewController.swift
+//  Pitch Perfect
+//
+//  Created by Zohaib Maqsood on 4/4/15.
+//  Copyright (c) 2015 Xeb. All rights reserved.
+//
+
+import Cocoa
+
+class MyViewController: NSObject {
+
+}
