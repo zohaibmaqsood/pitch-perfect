999299e52ed0be8db697b6602620a70e9f75ac93
zohaibmaqsood <maqsood.zohaib@gmail.com>
2015-04-05 15:24:55 -0500

    Committed by Udacity

---
+++ Pitch Perfect/Pitch Perfect/FileProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  FileProtocol.swift
+//  Pitch Perfect
+//
+//  Created by Zohaib Maqsood on 4/4/15.
+//  Copyright (c) 2015 Xeb. All rights reserved.
+//
+
+import Foundation
+
+protocol FileProtocol {
+    
+    func getFilePath() -> NSURL!;
+}
